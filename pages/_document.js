2b03e248e2ebaa64baeac141a0988f5343bc554a
Daniel Kapper <dkapper01@gmail.com>
2018-05-27 12:23:12 -0400

    before adding express

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import JssProvider from 'react-jss/lib/JssProvider';
 import Document, { Head, Main, NextScript } from 'next/document';
+
+import getContext from '../lib/context';
 
 class MyDocument extends Document {
   render() {
@@ -18,7 +22,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Muli:300,400:latin"
           />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
           <link
             rel="stylesheet"
             href="https://storage.googleapis.com/builderbook/nprogress.min.css"
@@ -43,16 +50,15 @@ class MyDocument extends Document {
                 border-left: 0.25em solid #dfe2e5;
               }
               pre {
-                display: block;
-                overflow-x: auto;
-                padding: 0.5em;
-                background: #FFF;
+                display:block;
+                overflow-x:auto;
+                padding:0.5em;
+                background:#FFF;
+                color: #000;
                 border: 1px solid #ddd;
               }
               code {
                 font-size: 14px;
-                background: #FFF;
-                padding: 3px 5px;
               }
             `}
           </style>
@@ -74,5 +80,31 @@ class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = ({ renderPage }) => {
+  const pageContext = getContext();
+  const page = renderPage(Component => props => (
+    <JssProvider
+      registry={pageContext.sheetsRegistry}
+      generateClassName={pageContext.generateClassName}
+    >
+      <Component pageContext={pageContext} {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    pageContext,
+    styles: (
+      <style
+        id="jss-server-side"
+        // eslint-disable-next-line
+        dangerouslySetInnerHTML={{
+          __html: pageContext.sheetsRegistry.toString(),
+        }}
+      />
+    ),
+  };
+};
 
 export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Button from 'material-ui/Button';
 
 import withLayout from '../lib/withLayout';
 
@@ -7,12 +6,9 @@ const Index = () => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>
-      <meta name="description" content="This is description of Index page" />
+      <meta name="description" content="This is SEO description of Index page" />
     </Head>
     <p>Content on Index page</p>
-    <Button variant="raised">
-      MUI button
-    </Button>
   </div>
 );
 
